ac3f98c2508053002e472779bf7d9a98ecd91372
JianCong <129957211+JianCong03@users.noreply.github.com>
2024-06-11 14:17:01 +0800

    Add files via upload

--- SoftwareEngineering/sign_up.py
+++ SoftwareEngineering/sign_up.py
@@ -34,6 +34,7 @@ def check_sign_up():
     email = insert_email.get()
     gender = gender_type_combobox.get()
     blood = blood_type_var.get()
+    age = insert_age.get()
 
     # if email don't have @ error
     if '@' not in email:
@@ -46,8 +47,9 @@ def check_sign_up():
         docRef = db.collection('user').document(username)
         docRef.set({
             'username': username,
-            'password': password,
+            'age': age,
             'email': email,
+            'password': password,
             'gender': gender,
             'bloodType': blood,
         })
@@ -60,36 +62,41 @@ def check_sign_up():
 
 def sign():
     # name, email, password, gender, age, blood type
-    global insert_username, insert_password, insert_email, gender_type_combobox, blood_type_var
-    name = Label(patient_sign_up, text="Name          :", font=('Arial', 20))
-    name.place(x=200, y=190)
+    global insert_username, insert_age, insert_email, insert_password, gender_type_combobox, blood_type_var
+    name = Label(patient_sign_up, text="Name         :", font=('Arial', 20))
+    name.place(x=200, y=180)
     insert_username = Entry(patient_sign_up, width=30, font='Arial 19')
-    insert_username.place(x=380, y=195)
+    insert_username.place(x=380, y=180)
 
-    password = Label(patient_sign_up, text="Password    :", font=('Arial', 20))
-    password.place(x=200, y=290)
-    insert_password = Entry(patient_sign_up, width=30, font=('Arial', 19))
-    insert_password.place(x=380, y=290)
+    age = Label(patient_sign_up, text="Age            :", font=('Arial', 20))
+    age.place(x=200, y=260)
+    insert_age = Entry(patient_sign_up, width=30, font=('Arial', 19))
+    insert_age.place(x=380, y=260)
 
     email = Label(patient_sign_up, text="Email          :", font=('Arial', 20))
-    email.place(x=200, y=390)
+    email.place(x=200, y=340)
     insert_email = Entry(patient_sign_up, width=30, font=('Arial', 19))
-    insert_email.place(x=380, y=390)
+    insert_email.place(x=380, y=340)
+
+    password = Label(patient_sign_up, text="Password    :", font=('Arial', 20))
+    password.place(x=200, y=420)
+    insert_password = Entry(patient_sign_up, width=30, font=('Arial', 19))
+    insert_password.place(x=380, y=420)
 
     gender = Label(patient_sign_up, text="Gender       :", font=('Arial', 20))
-    gender.place(x=200, y=490)
+    gender.place(x=200, y=510)
     gender_types = ["Male", "Female"]
     gender_type_combobox = Combobox(patient_sign_up, values=gender_types, font=('Arial', 19), state='readonly')
-    gender_type_combobox.place(x=380, y=490)
+    gender_type_combobox.place(x=380, y=510)
 
-    blood_type = Label(patient_sign_up, text="Blood Type :", font=('Arial', 20))
-    blood_type.place(x=200, y=590)
+    blood_type = Label(patient_sign_up, text="Blood Type  :", font=('Arial', 20))
+    blood_type.place(x=200, y=600)
     blood_types = ["A", "B", "AB", "O"]
     blood_type_var = StringVar()
     for i, blood_type_option in enumerate(blood_types):
         blood_type_radio = Radiobutton(patient_sign_up, text=blood_type_option, variable=blood_type_var,
                                        value=blood_type_option, font=('Arial', 19))
-        blood_type_radio.place(x=380 + i * 100, y=590)
+        blood_type_radio.place(x=380 + i * 100, y=600)
 
     submit = Button(patient_sign_up, text="Sign Up", height=2, width=30, font=('Arial', 20), command=check_sign_up)
     submit.place(x=250, y=690)
