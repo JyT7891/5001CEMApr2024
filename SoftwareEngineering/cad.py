3eddc707d18e8b40be154a85b2487dbf95f7d97e
JyT7891 <129957186+JyT7891@users.noreply.github.com>
2024-06-21 21:20:00 +0800

    Delete SoftwareEngineering directory

--- SoftwareEngineering/cad.py
+++
@@ -1,5 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-cred = credentials.Certificate("C:/Users/ASUS/PycharmProjects/SoftwareEngineering/cad_firebase.json")
-firebase_admin.initialize_app(cred)
